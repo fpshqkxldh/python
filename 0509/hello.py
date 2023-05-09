2da5db48ff44987a534c142f84884351756cf281
Your Name <you@example.com>
2023-05-09 12:36:48 +0900

    오늘의 실습

---
+++ 0509/hello.py
@@ -0,0 +1,11 @@
+# 나만의 메소드 만들기
+
+def helloworld():
+    print("hello world!!")
+
+if __name__ == "__main__":
+    print("오늘은 모듈을 처음 배운 날입니다.")
+    print("오늘의 날씨는 24도입니다.")
+    helloworld()
+else:
+    print("__name__ in hello.py : ", __name__)
